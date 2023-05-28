ed21c31cff28b3be41fdaf7d7824e6505e27555d
Dody Nurrahman <dodynrhmn@gmail.com>
2023-05-28 21:34:10 +0800

    Buat Class Aksi

---
+++ src/praktek3/SepedaAksi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktek3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SepedaAksi {
+    public static void main(String[] args) {
+        
+    }
+    
+}
