eafecbb1bdfb325a05df8d55c74d228821a02173
Dody Nurrahman <dodynrhmn@gmail.com>
2023-05-28 21:39:57 +0800

    Instance, buat atribut dan panggil method

--- src/praktek3/SepedaAksi.java
+++ src/praktek3/SepedaAksi.java
@@ -10,7 +10,12 @@ package praktek3;
  */
 public class SepedaAksi {
     public static void main(String[] args) {
+        Sepeda s = new Sepeda();
+        s.merk="Kupu-kupu";
+        s.warna="Merah";
+        s.harga=1000.0;
         
+        s.cetakInfo();
     }
     
 }
